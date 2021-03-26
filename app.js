e692851c4b604cbe7cce803acb900195dd5d5c0c
dc\hidir.koh <hidir.koh@the-ascott.com>
2021-03-26 18:16:47 +0800

    Create server on port 3000

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const server = http.createServer((req,res)=>{
+    res.writeHead(200, {'content-type':'text/html'})
+    res.end('<h1> Hello World </h1>')
+})
+
+server.listen(3000, ()=>{
+    console.log('listening on port 3000')
+})
