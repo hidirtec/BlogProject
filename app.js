47b5ee6c9d2d5165192018909fda5b579d01a7bf
dc\hidir.koh <hidir.koh@the-ascott.com>
2021-03-26 22:28:08 +0800

    Get API

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require('http')
+const querystring = require('querystring')
+
 const server = http.createServer((req,res)=>{
+    console.log(req.method)     //GET
+    const url = req.url   
+    console.log(url)      
+    req.query = querystring.parse(url.split('?')[1])
+    console.log('query: ',req.query)
     res.writeHead(200, {'content-type':'text/html'})
-    res.end('<h1> Hello World </h1>')
+    res.end(JSON.stringify(req.query))
 })
 
 server.listen(3000, ()=>{
